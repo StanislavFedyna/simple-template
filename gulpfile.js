35f1775d61e13abc3a2eae42c2621c5c77ff3bc5
StanislavFedyna <38083655+StanislavFedyna@users.noreply.github.com>
2018-08-16 11:23:49 +0300

    changed sass task, update

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,24 @@ var gulp         = require('gulp'),
     browserSync  = require('browser-sync'), 
     csscomb      = require('gulp-csscomb');
 
-gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('sass/**/*.scss') // Берем источник
+gulp.task('sass', function(){ 
+    return gulp.src('sass/**/*.scss') 
         .pipe(csscomb())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css/')) // Выгружаем результата в папку src/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('css/')) 
+        .pipe(browserSync.reload({stream: true})) 
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: '../simpleTemplate/' // Директория для сервера - src
+gulp.task('browser-sync', function() { 
+    browserSync({ 
+        server: { 
+            baseDir: '../simpleTemplate/' 
         },
-        notify: false // Отключаем уведомления
+        notify: false 
     });
 });
 
 gulp.task('watch', ['browser-sync'], function() {
-    gulp.watch('sass/main.scss', ['sass']); // Наблюдение за sass файлами в папке sass
-    gulp.watch('*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-});
+    gulp.watch('sass/main.scss', ['sass']); 
+    gulp.watch('*.html', browserSync.reload); 
 
